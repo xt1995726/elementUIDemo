a1ede6193e58bbed5514cfd3f01082699da3cc4b
xt1995726 <503634677@qq.com>
2019-01-28 21:35:58 +0800

    form change

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -12,12 +11,8 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+body {
+    font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
+    background-color: #F2F2F2;
+  }
 </style>
--- src/components/form.vue
+++ src/components/form.vue
@@ -1,40 +1,81 @@
 <template>
-<section class="form-section">
-  <el-form ref="ruleForm" label-width="100px" class="demo-ruleForm">
-    <el-form-item label="待办事项" prop="name">
-      <el-input></el-input>
-    </el-form-item>
-    <el-form-item label="事项类型" prop="type">
-      <el-radio-group>
-        <el-radio label="1">常规</el-radio>
-        <el-radio label="2">紧急</el-radio>
-      </el-radio-group>
-    </el-form-item>
-    <el-form-item label="提醒方式" prop="way">
-      <el-checkbox-group>
-        <el-checkbox label="短信"></el-checkbox>
-        <el-checkbox label="电话"></el-checkbox>
-        <el-checkbox label="邮件"></el-checkbox>
-        <el-checkbox label="微信"></el-checkbox>
-      </el-checkbox-group>
-    </el-form-item>
-  </el-form>
-</section>
+  <section class="form-section">
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="待办事项" prop="name">
+        <el-input v-model="ruleForm.name"></el-input>
+      </el-form-item>
+      <el-form-item label="事项类型" prop="type">
+        <el-radio-group v-model="ruleForm.type">
+          <el-radio label="1">常规</el-radio>
+          <el-radio label="2">紧急</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="提醒方式" prop="way">
+        <el-checkbox-group v-model="ruleForm.way">
+          <el-checkbox label="短信"></el-checkbox>
+          <el-checkbox label="电话"></el-checkbox>
+          <el-checkbox label="邮件"></el-checkbox>
+          <el-checkbox label="微信"></el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+      <el-form-item label="提醒时间" required>
+        <el-row>
+          <el-form-item prop="date">
+            <el-time-picker type="fixed-time" placeholder="选择提醒时间" v-model="ruleForm.date" style="width: 100%"></el-time-picker>
+          </el-form-item>
+        </el-row>
+      </el-form-item>
+
+      <el-form-item label="重复提醒" prop="delivery">
+        <el-switch on-text="on" off-text="off" v-model="ruleForm.delivery"></el-switch>
+      </el-form-item>
+      <el-form-item label="重复时间" :required="redStar">
+        <el-col :span="12">
+          <el-form-item prop="endDate">
+            <el-date-picker :disabled="forbidden" type="date" placeholder="选择结束日期" :picker-options="pickerOptionsOver" v-model="ruleForm.endDate" style="width: 100%;"></el-date-picker>
+          </el-form-item>
+
+        </el-col>
+      </el-form-item>
+    </el-form>
+  </section>
 </template>
 
 <script>
 export default {
   data() {
     return {
-
+      redStar: false,
+      ruleForm: {
+        name: '',
+        type: '',
+        way: [],
+        date: '',
+        delivery: false,
+      },
+      rules: {
+        name: [
+          { required: true, message: '请输入待办事项名称', trigger: 'blur' },
+          { min: 3, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur' }
+        ],
+        type: [
+          { required: true, message: '请选择代班事项类型', trigger: 'change' }
+        ],
+        way: [
+          { type: 'array', required: true, message: '请至少选择一个提醒方式', trigger: 'change' }
+        ],
+        date: [
+          { type: 'date', required: true, message: '请选择提醒时间', trigger: 'change' }
+        ],
+      },
     }
   }
 }
 </script>
 
 <style scoped>
-  .form-section{
-    padding: 10px;
-    width: 500px;
-  }
+.form-section {
+  padding: 10px;
+  width: 500px;
+}
 </style>
