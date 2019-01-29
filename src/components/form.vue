3bab321e914c9e1144ed3ff9fb10973e9e2751d5
xt1995726 <503634677@qq.com>
2019-01-29 21:40:33 +0800

    form finished

--- src/components/form.vue
+++ src/components/form.vue
@@ -27,15 +27,35 @@
       </el-form-item>
 
       <el-form-item label="重复提醒" prop="delivery">
-        <el-switch on-text="on" off-text="off" v-model="ruleForm.delivery"></el-switch>
+        <el-switch on-text="on" off-text="off" v-model="ruleForm.delivery" @change="changeOnOff"></el-switch>
       </el-form-item>
       <el-form-item label="重复时间" :required="redStar">
+        <el-col :span="11">
+          <el-form-item prop="starDate">
+            <el-date-picker :disabled="forbidden" type="date" placeholder="选择开始日期" :picker-options="pickerOptionsStart" v-model="ruleForm.starDate" @change="starTimeChange" style="width: 100%;"></el-date-picker>
+          </el-form-item>
+        </el-col>
+        <el-col class="line" :span="1">&nbsp;-</el-col>
         <el-col :span="12">
           <el-form-item prop="endDate">
-            <el-date-picker :disabled="forbidden" type="date" placeholder="选择结束日期" :picker-options="pickerOptionsOver" v-model="ruleForm.endDate" style="width: 100%;"></el-date-picker>
+            <el-date-picker :disabled="forbidden" type="date" placeholder="选择结束日期" :picker-options="pickerOptionsOver" v-model="ruleForm.endDate" @change="endTimeChange" style="width: 100%;"></el-date-picker>
           </el-form-item>
-
         </el-col>
+      </el-form-item>
+      <el-form-item label="重复周期" prop="period" :required="redStar">
+        <el-select :disabled="forbidden" v-model="ruleForm.period" placeholder="请选择重复周期">
+          <el-option label="每日提醒" value="1"></el-option>
+          <el-option label="每周提醒" value="2"></el-option>
+          <el-option label="每月提醒" value="3"></el-option>
+          <el-option label="工作日提醒" value="4"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="其他信息" prop="other">
+        <el-input type="textarea" v-model="ruleForm.other"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
       </el-form-item>
     </el-form>
   </section>
@@ -45,13 +65,37 @@
 export default {
   data() {
     return {
+      period: [
+        { required: true, message: '请选择重复周期', trigger: 'change' }
+      ],
+      starDate: [
+        { type: 'date', required: true, message: '请选择开始日期', trigger: 'change' }
+      ],
+      endDate: [
+        { type: 'date', required: true, message: '请选择结束日期', trigger: 'change' }
+      ],
+      pickerOptionsStart: {
+        disabledDate(time) {
+          return time.getTime() < 1546272000000 || time.getTime() >= Date.now();
+        }
+      },
+      pickerOptionsOver: {
+        disabledDate(time) {
+          return time.getTime() < 1546272000000 || time.getTime() >= Date.now();
+        }
+      },
+      forbidden: true,
       redStar: false,
       ruleForm: {
         name: '',
         type: '',
         way: [],
         date: '',
+        peroid: '',
+        starDate: '',
+        endDate: '',
         delivery: false,
+        other: '',
       },
       rules: {
         name: [
@@ -67,7 +111,76 @@ export default {
         date: [
           { type: 'date', required: true, message: '请选择提醒时间', trigger: 'change' }
         ],
+        peroid: [],
+        starDate: [],
+        endDate: [],
+        other: [
+          { required: true, message: '请填写其他信息', trigger: 'blur' }
+        ]
       },
+    }
+  },
+  methods: {
+    getDate(strDate) {
+      let st = strDate;
+      let a = st.split(" ");
+      let b = a.split("-");
+      let date = new Date(b[0], b[1] - 1, b[2]);
+      return date;
+    },
+    // 开始时间禁止晚于结束时间
+    starTimeChange(val) {
+      const starTime = this.getDate(val);
+      this.pickerOptionsOver = {
+        disabledDate(time) {
+          return time.getTime() <= starTime || time.getTime() >= Date.now();
+        }
+      }
+    },
+    // 结束时间禁止早于开始时间
+    endTimeChange(val) {
+      let endTime = this.getDate(val);
+      this.pickerOptionsStart = {
+        disabledDate(time) {
+          return time.getTime() < 1546272000000 || time.getTime() >= endTime;
+        }
+      }
+    },
+    changeOnOff(val) {
+      // 对重复提醒的开关，给表单加rules，还有红色星号
+      if (val === true) {
+        this.forbidden = false;
+        this.redStar = true;
+        this.$set(this.rules, "starDate", this.starDate);
+        this.$set(this.rules, "endDate", this.endDate);
+        this.$set(this.rules, "period", this.period);
+        this.$refs['ruleForm'].validate();
+      } else {
+        // 关闭重复提醒功能
+        this.forbidden = true;
+        this.redStar = false;
+        this.$set(this.rules, "starDtae", [{ type: 'date', required: false, trigger: 'change' }]);
+        this.$set(this.rules, "endDtae", [{ type: 'date', required: false, trigger: 'change' }]);
+        this.$set(this.rules, "period", [{}]);
+        this.$refs['ruleForm'].validate();
+      }
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          let para = Object.assign({}, this.ruleForm);
+          console.log(para);
+          this.$message({
+            message: "提交成功",
+            type: "success"
+          });
+        } else {
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
     }
   }
 }
